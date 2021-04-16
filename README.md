a1645727fa8e988f304bef8fb1bf25fba3974cc9
ningzichun <mrnzc@foxmail.com>
2021-04-16 22:00:18 +0800

    文档 needed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Hi there 👋
+### 摸渔🐟，another 网上购物系统。
 
 <!--
 **touchfishing/touchfishing** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
--- backend/touchfishing/scripts/install.sh
+++ backend/touchfishing/scripts/install.sh
@@ -3,8 +3,9 @@ echo "超级管理员设置"
 read -p "用户名：" superuser
 read -p "密码：" superpass
 read -p "邮箱：" supermail
-apt install python3 python3-pip  build-essential -y
+#apt install python3 python3-pip  build-essential -y
 pip3 install -r requirements.txt
+cd ..
 python3 manage.py makemigrations
 python3 manage.py makemigrations user
 python3 manage.py makemigrations mall
